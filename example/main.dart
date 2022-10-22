3a39d93d25b9cc244084f4c176e70d0131721f33
MedzikUser <medzik@duck.com>
2022-10-22 22:38:10 +0200

    chore: update examples

--- example/main.dart
+++ example/main.dart
@@ -1,8 +1,30 @@
+import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:libcrypto/libcrypto.dart';
 
 void main() async {
+  print('Pbkdf2:');
+  await pbkdf2Example();
+
+  print('\nAES-CBC:');
+  await aesCbcExample();
+}
+
+Future<void> pbkdf2Example() async {
+  final secret = 'P@ssw0rd1234!';
+  final salt = Uint8List.fromList(utf8.encode('example@example.com'));
+
+  final hasher = Pbkdf2(iterations: 1000);
+
+  final sha256Hash = await hasher.sha256(secret, salt);
+  print('sha256: $sha256Hash');
+
+  final sha512Hash = await hasher.sha512(secret, salt);
+  print('sha512: $sha512Hash');
+}
+
+Future<void> aesCbcExample() async {
   final clearText = 'hello world';
   final secret = 'P@ssw0rd1234!';
   final salt = Uint8List.fromList(Salt(20).generate());
